2768942c17bfba4601d63bd8029793d8227e7642
fanian <rfanian@gmail.com>
2014-12-02 13:15:55 +0300

    add scaffolding header and nav

---
+++ js/init.js
@@ -0,0 +1,6 @@
+head.load("js/lib/jquery.js",
+					"js/common.js",
+                    "js/lib/jquery.validate.js",
+                    "js/lib/jquery.form.js",
+                    "js/lib/bootstrap.js"
+                     );
