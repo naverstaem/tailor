f2802bfa6ffb2b3888b33e95e2b5e216ba566ed6
fanian <rfanian@gmail.com>
2014-12-02 22:20:39 +0300

    small fixes

--- js/common.js
+++ js/common.js
@@ -47,12 +47,44 @@ head.ready(function() {
     //});
 
 
+    // Normalize Carousel Heights - pass in Bootstrap Carousel items.
+    $.fn.carouselHeights = function() {
+
+        var items = $(this), //grab all slides
+            heights = [], //create empty array to store height values
+            tallest; //create variable to make note of the tallest slide
+
+        var normalizeHeights = function() {
+
+            items.each(function() { //add heights to array
+                heights.push($(this).height());
+            });
+            tallest = Math.max.apply(null, heights); //cache largest value
+            items.each(function() {
+                $(this).css('min-height',tallest + 'px');
+            });
+        };
+
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            //reset vars
+            tallest = 0;
+            heights.length = 0;
+
+            items.each(function() {
+                $(this).css('min-height','0'); //reset min-height
+            });
+            normalizeHeights(); //run it again
+        });
+
+    };
+
     $('#form1').validate();
     $('#form2').validate();
     $('#form3').validate();
     $('#form4').validate();
     $('#form5').validate();
-    $('#form6').validate();
 
     //$("#form5").ajaxForm({
     //    success: function () {
